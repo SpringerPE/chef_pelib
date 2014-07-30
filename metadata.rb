5ce37af8ce604a739021e03d0da2da7d6fdbb1e5
jose.riguera@springer.com <jose.riguera@springer.com>
2014-07-30 10:34:10 +0200

    init

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'pelib'
+maintainer       'Springer Platform Engineering'
+maintainer_email 'platform-engineering@springer.com'
+license          'Apache 2.0'
+description      'Network and FW configuration for servers'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
