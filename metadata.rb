356a3e54f055b3733ebed0d6b2e8a3ce78ab26c1
jose.riguera@springer.com <jose.riguera@springer.com>
2014-08-05 11:41:08 +0200

    updated metadata

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'pelib'
 maintainer       'Springer Platform Engineering'
 maintainer_email 'platform-engineering@springer.com'
 license          'Apache 2.0'
-description      'Network and FW configuration for servers'
+description      'Base Cookbook library'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
